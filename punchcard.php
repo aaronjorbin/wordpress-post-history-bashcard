1912ffc4ed4a2328473c0625b1d70d7b2ccf62b3
Aaron Jorbin <aaron@jorb.in>
2013-03-03 21:11:33 -0500

    add wp_error to parser file and output dates

--- punchcard.php
+++ punchcard.php
@@ -4,9 +4,6 @@ require "parser.php";
 $file = $argv[1];;
 $parser = new WXR_Parser();
 $data = $parser->parse( $file );
-$dataFile = fopen('datafile', 'w');
 foreach($data["posts"] as $post){
-    fwrite($dataFile,  '#'.strtotime($post["post_date"])."\n");
+    echo strtotime($post["post_date"])."\n";
 }
-fclose($dataFile);
-
